4f7cbbc0f193ff592c4b1b042d5d93fb08d26efc
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-12-03 19:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,16 +12,18 @@ class Animal {
 }
 
 class Dog extends Animal {
-
-	purr(){
-		console.log("purr")
+bark(){
+		console.log("woof")
 	}
+
 }
 
 class Cat extends Animal {
-	bark(){
-		console.log("woof")
+
+		purr(){
+		console.log("purr")
 	}
+	
 }
 
 const myCat = new Cat("Siamese");
